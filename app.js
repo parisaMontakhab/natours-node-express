962d1895db8781ad266d4b2d0e947e7de8ba6597
Parisa Montakhabi <parisa.montakhabi@gmail.com>
2026-05-19 12:11:03 +0200

    feat : handling Post method with express

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 
 const app = express();
 
+app.use(express.json());
+
 // handling get method
 
 const tours = JSON.parse(
@@ -12,6 +14,21 @@ const tours = JSON.parse(
 
 app.get('/api/tours', (req, res) => {
   res.status(200).json({ status: 'success', data: { tours } });
+});
+
+app.post('/api/tours', (req, res) => {
+  const newID = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newID }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({ status: 'success', data: { tour: newTour } });
+    },
+  );
 });
 
 const port = 3000;
