21ad68043a1cb570192cc505b83535518a0101e9
Parisa Montakhabi <parisa.montakhabi@gmail.com>
2026-05-24 09:59:52 +0200

    feat : install mongoose ad configured .env to connect database

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 //middleware
-app.use(morgan('dev'));
+
+if ((process.env.NODE_ENV = 'development')) {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 
 // routes
