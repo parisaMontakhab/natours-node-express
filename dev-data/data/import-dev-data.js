e4df6f217ccb372f6ebcc11a4cc85dd220c4249d
Parisa Montakhabi <parisa.montakhabi@gmail.com>
2026-05-25 16:23:34 +0200

    feat : import all data of tours-simple.js file to database with command

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModels');
+const { json } = require('stream/consumers');
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
+
+//READ JSON FILE
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+);
+
+//IMPORT DATA TO DATABASE
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('data successfully loaded');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+//DELETE ALL DATA FROMM COLLECTION
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('data successfully deleted');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
