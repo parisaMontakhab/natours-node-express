2a9cc0b2ae0b49256871bef8311cb4f5dbfedb6a
Parisa Montakhabi <parisa.montakhabi@gmail.com>
2026-05-30 09:59:38 +0200

    feat : adding pagination

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -42,6 +42,18 @@ exports.getAllTour = async (req, res) => {
       query = query.select('-__v');
     }
 
+    //PAGINATION
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     //EXECUTE QUERY
     const tours = await query;
 
