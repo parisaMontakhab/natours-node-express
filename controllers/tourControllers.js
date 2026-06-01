d5313fca86c4b1ec4b65fa97bcb28d14cd7568b4
Parisa Montakhabi <parisa.montakhabi@gmail.com>
2026-06-01 12:40:57 +0200

    feat : add aggregation pipeline

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -1,62 +1,25 @@
 const Tour = require('../models/tourModels');
+const APIFeatures = require('../utils/apiFeatures');
+
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+
+  next();
+};
 
 exports.getAllTour = async (req, res) => {
   try {
-    //const tours = await Tour.find();
-
-    // const tours = await Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');
-
-    //FILTERING
-    const queryObj = { ...req.query };
-
-    const excludeFields = ['page', 'sort', 'fields', 'limit'];
-    excludeFields.forEach((el) => delete queryObj[el]);
-
-    //ADVANCE FILTERING
-
-    let queryString = JSON.stringify(queryObj);
-    queryString = queryString.replace(
-      /\b(gte|gt|lte|lt)\b/g,
-      (match) => `$${match}`,
-    );
-
-    let query = Tour.find(JSON.parse(queryString));
-
-    //SORTING
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    //LIMITING
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    //PAGINATION
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exist');
-    }
-
     //EXECUTE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
+    //SEND RESPONSE
     res.status(200).json({
       status: 'success',
       result: tours.length,
@@ -123,5 +86,36 @@ exports.createNewTour = async (req, res) => {
       status: 'fail',
       message: err,
     });
+  }
+};
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: '$difficulty',
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        $sort: { avgPrice: 1 },
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: { stats },
+    });
+  } catch (err) {
+    res.status(404).json({ status: 'fail', message: err });
   }
 };
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,8 +5,14 @@ const {
   getTour,
   updateTour,
   deleteTour,
+  aliasTopTours,
+  getTourStats,
 } = require('../controllers/tourControllers');
 const router = express.Router();
+
+router.route('/top-5-cheap').get(aliasTopTours, getAllTour);
+
+router.route('/tour-stats').get(getTourStats);
 
 router.route('/').get(getAllTour).post(createNewTour);
 
