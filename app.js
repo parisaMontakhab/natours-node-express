b51578ac74628402be6a68c6d348a82acb7901e0
Parisa Montakhabi <parisa.montakhabi@gmail.com>
2026-06-10 13:25:50 +0200

    feat : implement globalHandleError

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // requires
 const express = require('express');
 const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorControllers');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -18,5 +21,13 @@ app.use(express.json());
 
 app.use('/api/tours', tourRouter);
 app.use('/api/users', userRouter);
+
+// Global error handling
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl}  ...`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
