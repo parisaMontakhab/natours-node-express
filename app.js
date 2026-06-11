66437793d1baa4da0fb8c5c31a6e72de8a449f70
Parisa Montakhabi <parisa.montakhabi@gmail.com>
2026-06-11 15:12:45 +0200

    feat : adding Duplicate Error handling

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.set('query parser', 'extended');
 //middleware
 
-if ((process.env.NODE_ENV = 'development')) {
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
--- controllers/errorControllers.js
+++ controllers/errorControllers.js
@@ -1,9 +1,61 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path} : ${err.value} `;
+
+  return new AppError(message, 400);
+};
+
+const handleDuplicateErrorDB = (err) => {
+  const value = Object.values(err.keyValue)[0];
+
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorPro = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error('Error', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
+  console.log('ENV IS:', process.env.NODE_ENV);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    error.message = err.message;
+    error.name = err.name;
+    error.code = err.code;
+    error.keyValue = err.keyValue;
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateErrorDB(error);
+
+    sendErrorPro(error, res);
+  }
 };
