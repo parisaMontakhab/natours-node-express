711097f355e2cb4671af9f03aa2d4697dcf55b75
Parisa Montakhabi <parisa.montakhabi@gmail.com>
2026-07-02 19:34:49 +0200

    feat : implemented data sanitization

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorControllers');
@@ -32,6 +34,12 @@ app.use('/api', limiter);
 
 //body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+//data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+//data sanitization against XXS
+app.use(xss());
 
 // routes
 
