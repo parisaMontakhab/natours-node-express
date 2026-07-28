1670710fd788cde03a021e64e42a6ac3bd8f073a
Parisa Montakhabi <parisa.montakhabi@gmail.com>
2026-07-28 12:23:29 +0200

    feat : implemented tour and overview  pugs

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,9 +1,22 @@
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+const Tour = require('../models/tourModels');
 
-exports.getOverview = catchAsync(async (req, res) => {
-  res.status(200).render('overview', { title: 'All Tours' });
+exports.getOverview = catchAsync(async (req, res, next) => {
+  const tours = await Tour.find();
+  res.status(200).render('overview', { title: 'All Tours', tours });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
-  res.status(200).render('tour', { title: 'The forest hiker' });
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findOne({
+    slug: req.params.slug,
+  }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+
+  if (!tour) {
+    return next(new AppError('There is no tour with that name.', 404));
+  }
+  res.status(200).render('tour', { title: tour.name, tour });
 });
