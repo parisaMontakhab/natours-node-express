37fc2185cd464c91d963a2226c694d9acf310961
Parisa Montakhabi <parisa.montakhabi@gmail.com>
2026-07-28 12:24:38 +0200

    refactor : changed the title of the detail page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -18,5 +18,5 @@ exports.getTour = catchAsync(async (req, res, next) => {
   if (!tour) {
     return next(new AppError('There is no tour with that name.', 404));
   }
-  res.status(200).render('tour', { title: tour.name, tour });
+  res.status(200).render('tour', { title: `${tour.name} Tour`, tour });
 });
