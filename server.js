0d4a32d53a64dec3f29cfeba5c9150d228ef7392
Parisa Montakhabi <parisa.montakhabi@gmail.com>
2026-08-07 18:15:36 +0200

    fix: add fallback port

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const DB = process.env.DATABASE.replace(
 
 mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
